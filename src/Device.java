f90c387ab16bd815aeabed8ed5cd9b7985b01378
yonose <yonoverse@gmail.com>
2026-01-29 14:44:56 -0500

    Making things neater

--- src/Device.java
+++ src/Device.java
@@ -5,11 +5,13 @@ public class Device {
     static Parser printMate = new Parser();
     static Scanner userInput = new Scanner(System.in);
     static void main() {
-        System.out.println("What is the ID?");
-        String newestID = userInput.nextLine();
-        String testNeighbor = findNeighbors().get("S1");
+        System.out.println("What is the ID for Config Info?");
+        String newestIDConfig = userInput.nextLine();
+        System.out.println("What is the ID for finding Neighbors?");
+        String newestIDNeighbor = userInput.nextLine();
+        String testNeighbor = findNeighbors().get(newestIDNeighbor);
         System.out.println("Your Neighbors are: " + testNeighbor);
-        Config testConfig = getConfig().get(newestID);
+        Config testConfig = getConfig().get(newestIDConfig);
         System.out.println("Your Config information is" + testConfig);
     }
     static public void DeviceClass (String ID, String ports, String IP){
