111d7c8c3d9a8248681c7adf6320cab58cd4bb1f
Basically654 <ikanke04@gmail.com>
2026-01-31 20:00:33 -0500

    Create Packet class

--- src/Packet.java
+++ src/Packet.java
@@ -1,7 +1,14 @@
+import java.io.IOException;
+import java.net.*;
 
-
-public class Packet {
+public class Packet{
     //datatype for packet that'll carry 4 parameters
+    public void udpPacket(String message, int port, String ip) throws IOException {
+        DatagramSocket client = new DatagramSocket();
+        InetAddress addr = InetAddress.getByName(ip);
+        byte[] buf = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, addr, port);
 
-
+        client.send(packet);
+    }
 }
