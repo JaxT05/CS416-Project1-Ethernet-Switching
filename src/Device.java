7bee9ed6cb18ea2dcf80b4400d00e4f90a7c7047
yonose <yonoverse@gmail.com>
2026-02-19 10:38:09 -0500

    Completing the parser

--- src/Device.java
+++ src/Device.java
@@ -2,20 +2,36 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Device {
-    static Parser printMate = new Parser();
-    static Scanner userInput = new Scanner(System.in);
+    String ID;
+    String Port;
+    String IP;
+    String GatIP;
+    String GatIP2;
+    String VirIP;
 
-    public static void main(String[] args) {
-        System.out.println("What is the ID for Config Info?");
-        String newestIDConfig = userInput.nextLine();
-        String testConfig = getConfig().get(newestIDConfig);
-        System.out.println("Your Config information is " + testConfig);
-    }
-    static public void DeviceClass(String ID, String ports, String IP) {
-    }
-
-    public static Map<String, String> getConfig() {
-        return Parser.getConfigInfo();
+    public Device(String ID, String port, String IP, String gatIP, String gatIP2, String virIP) {
+        this.ID = ID;
+        Port = port;
+        this.IP = IP;
+        GatIP = gatIP;
+        GatIP2 = gatIP2;
+        VirIP = virIP;
 
     }
+
+    public Device(String ID, String port, String IP,  String gatIP, String virIP) {
+        this.ID = ID;
+        Port = port;
+        this.IP = IP;
+        GatIP = gatIP;
+        this.VirIP = virIP;
+    }
+
+    public Device(String ID, String port, String IP) {
+        this.ID = ID;
+        Port = port;
+        this.IP = IP;
+    }
+
+
 }
