4789721dcb2aca282c65a7b86ee0781cbd1836e6
Basically654 <ikanke04@gmail.com>
2026-02-22 16:31:17 -0500

    finish Host class and final iteration commit

--- src/Host.java
+++ src/Host.java
@@ -5,31 +5,39 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
 
+/*
+HOST CLASS MODIFICATIONS
+---------------------------------------
+i. Virtual source MAC address
+ii. Virtual destination MAC address
+iii. Virtual source IP address
+iv. Virtual destination IP address
+v. Short message
+
+Example: A:R1:net1.A:net3.D:hello!
+*/
+
 public class Host {
     public static void main(String[] args) throws Exception {
         //run the parser to figure out neighbors'
         Scanner scanner = new Scanner(System.in);
         System.out.print("ID: ");
         String ID = scanner.nextLine().trim();
-
-        String config = Parser.getConfigInfo().get(ID);
-        String[] configArray = config.split(">");
-        String vIP = Parser.getAllVIP(configArray[0])[0];
-        String hostSubnet = vIP.split("\\.")[0];
-
-        String gIP = Parser.getAllVIP(configArray[1])[0];
-        Map<String, String> nearestNeighbors = Parser.getNeighbors(configArray[2]);
-
+        String hostNeighbors = Parser.getConfigInfo().get(ID);
+        String hostConfig = Parser.getConfigInfo().get(ID);
+        String[] configArray = hostConfig.split(">");
+        String gateIP = configArray[1];
 
         int port = Integer.parseInt(args[1]);
         ArrayList<String> nearestPorts = new ArrayList<>();
+        Map<String, String> nearestNeighbors = Parser.getNeighbors(configArray[2]);
+        System.out.println(nearestNeighbors);
         DatagramSocket hostSocket = new DatagramSocket(port);
 
         for (String neighbor : nearestNeighbors.keySet()) {
             String neighborConfig = nearestNeighbors.get(neighbor);
             nearestPorts.add(neighborConfig);
         }
-//        System.out.println(nearestPorts);
 
         //creates a singular separate thread instead of establishing a pool of threads
         new Thread(() -> {
@@ -48,23 +56,23 @@ public class Host {
                 break;
             }
 
-            System.out.print("Destination IP: ");
-            String destinationIP = scanner.nextLine().trim();
-            String[] destinationIPArray = destinationIP.split("\\.");
-            String destinationSubnet = destinationIPArray[0];
-            String destinationMAC = "";
-
-            if (destinationSubnet.equalsIgnoreCase(hostSubnet)) {
-                destinationMAC = destinationIPArray[1];
-            }
-            else {
-                destinationMAC = gIP.split("\\.")[1];
-            }
-
+            System.out.print("Destination: ");
+            String destination = scanner.nextLine().trim();
             String nextDest = nearestPorts.getFirst();
 
-            String frame = ID + ":" + destinationMAC + ":" + vIP + ":" + destinationIP  + ":" + message;
-            sendFrame(hostSocket, nextDest, frame);
+            String virtualSourceAddr = getSourceAddr(ID);
+
+            //if the destination is not in the subnet send it to the router
+            if(!isSameNetwork(virtualSourceAddr, destination)){
+
+            }
+
+            String newFrame = ID+ ":" + gateIP + ":" + virtualSourceAddr + ":" +destination  + ":" + message;
+            String frame = ID+ ":"+ destination  + ":" + message;
+            System.out.println("Frame: " + newFrame);
+            if(!hostNeighbors.contains(destination)){
+                sendFrame(hostSocket, nextDest, newFrame);
+            }
         }
         hostSocket.close();
     }
@@ -74,10 +82,11 @@ public class Host {
             DatagramPacket incomingPacket = new DatagramPacket(new byte [1024], 1024);
             hostSocket.receive(incomingPacket);
             String frame = new String(incomingPacket.getData(), 0, incomingPacket.getLength()).trim();
+
             String[] frameContents = frame.split(":");
-            String sourceDeviceID = frameContents[3].split("\\.")[1];
+            String sourceDeviceID = frameContents[1];
             String destinationDeviceID = frameContents[2];
-            String message = frameContents[frameContents.length-1];
+            String message = frameContents[3];
             if (destinationDeviceID.equalsIgnoreCase(ID)) {
                 System.out.printf("%s, Source: %s\n", message, sourceDeviceID);
             }
@@ -89,6 +98,7 @@ public class Host {
 
     public static void sendFrame(DatagramSocket hostSocket, String nextDest, String frame) throws Exception {
         String[] nextDestArray = nextDest.split(" ");
+//        System.out.println(Arrays.toString(nextDestArray));
         InetAddress nextDestIP = InetAddress.getByName(nextDestArray[0]);
         int nextDestPort = Integer.parseInt(nextDestArray[1]);
         DatagramPacket outgoingPacket = new DatagramPacket(
@@ -98,5 +108,23 @@ public class Host {
                 nextDestPort
         );
         hostSocket.send(outgoingPacket);
+    }
+
+    public static boolean isSameNetwork(String s1, String s2){
+        String net1 = s1.split("\\.")[0];
+        String net2 = s2.split("\\.")[0];
+        return net1.equals(net2);
+    }
+
+    public static String getSourceAddr(String id){
+        String sourceAddress = "";
+
+        if(id.contains("A") || id.contains("B")){
+            sourceAddress = "net1." + id;
+        }
+        if (id.contains("C") || id.contains("D")){
+            sourceAddress = "net3." + id;
+        }
+        return sourceAddress;
     }
 }
